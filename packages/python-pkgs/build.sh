0377c02d2e26d57c67a0f2ff7e8c4979f23aad00
kawanakaiku <80381779+kawanakaiku@users.noreply.github.com>
2022-08-29 21:52:41 +0900

    pip

--- packages/python-pkgs/build.sh
+++ packages/python-pkgs/build.sh
@@ -7,8 +7,8 @@ TERMUX_PKG_VERSION=2022.08.25
 # TERMUX_PKG_BUILD_DEPENDS="python, freetype, libjpeg-turbo, libpng, portmidi, sdl2, sdl2-image, sdl2-mixer, sdl2-ttf, ffmpeg"
 # TERMUX_PKG_BUILD_DEPENDS="python, glu, freeglut, mesa"
 # TERMUX_PKG_BUILD_DEPENDS="python, mesa, glib, gstreamer, sdl2, sdl2-image, sdl2-mixer, sdl2-ttf"
-TERMUX_PKG_BUILD_DEPENDS="python, libopenblas, libgeos, ffmpeg"
-#TERMUX_PKG_BUILD_DEPENDS="python"
+#TERMUX_PKG_BUILD_DEPENDS="python, libopenblas, libgeos, ffmpeg"
+TERMUX_PKG_BUILD_DEPENDS="python"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_SKIP_SRC_EXTRACT=true
 
@@ -199,6 +199,7 @@ termux_step_pre_configure() {
 	PYTHON_PKGS=( bcrypt homeassistant orjson sqlalchemy )
 	PYTHON_PKGS=( scipy )
 	PYTHON_PKGS=( scikit-learn scikit-image shapely yt-dlp )
+	PYTHON_PKGS=( pip )
 	
 	
 	PYTHON_PKGS_OK=( )
