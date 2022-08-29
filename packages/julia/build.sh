fafea73c35f02dcbb4d4ed8111e817d54c1d9380
kawanakaiku <80381779+kawanakaiku@users.noreply.github.com>
2022-08-29 23:13:45 +0900

    Create build.sh

---
+++ packages/julia/build.sh
@@ -0,0 +1,6 @@
+TERMUX_PKG_HOMEPAGE=https://julialang.org/
+TERMUX_PKG_DESCRIPTION="The Julia Programming Language"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="@kawanakaiku"
+TERMUX_PKG_VERSION=1.8.0
+TERMUX_PKG_SRCURL=https://github.com/JuliaLang/julia.git
