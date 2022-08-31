57dddcb632f3b6c84faaa767cb4aa5b4beb6de09
kawanakaiku <80381779+kawanakaiku@users.noreply.github.com>
2022-08-31 14:30:39 +0900

    Create build.sh

---
+++ packages/code-server/build.sh
@@ -0,0 +1,12 @@
+TERMUX_PKG_HOMEPAGE=https://coder.com/
+TERMUX_PKG_DESCRIPTION="VS Code in the browser"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="kawanakaiku"
+TERMUX_PKG_VERSION=4.6.0
+TERMUX_PKG_SRCURL=https://github.com/coder/code-server.git
+ TERMUX_PKG_DEPENDS="nodejs"
+TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_make_install() {
+	./install.sh --prefix=${TERMUX_PREFIX}
+}
