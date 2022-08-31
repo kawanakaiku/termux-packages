3930c73e828e5b07112f65c77956c5b668e77b74
kawanakaiku <80381779+kawanakaiku@users.noreply.github.com>
2022-08-31 15:44:18 +0900

    Update build.sh

--- packages/code-server/build.sh
+++ packages/code-server/build.sh
@@ -4,16 +4,26 @@ TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="kawanakaiku"
 TERMUX_PKG_VERSION=4.6.0
 TERMUX_PKG_SRCURL=https://github.com/coder/code-server.git
- TERMUX_PKG_DEPENDS="nodejs"
+TERMUX_PKG_DEPENDS="nodejs"
+TERMUX_PKG_BUILD_DEPENDS="yarn"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure() {
 	termux_setup_nodejs
-	npm install --prefix=$TERMUX_COMMON_CACHEDIR yarn
-	# find / -name yarn 2>/dev/null
-	PATH="$TERMUX_COMMON_CACHEDIR/node_modules/yarn/bin:$PATH"
+	
+	local bin=$TERMUX_PKG_BUILDDIR/_bin
+	mkdir -p $bin
+	
+	local yarn=$bin/yarn
+	cat > $yarn <<-EOF
+		#!$(command -v sh)
+		exec sh $TERMUX_PREFIX/bin/yarn "\$@"
+		EOF
+	chmod 0755 $yarn
+
+	export PATH=$bin:$PATH
 }
 
 termux_step_make_install() {
-	yarn add --cwd ${TERMUX_PREFIX} .
+	yarn add --cwd ${TERMUX_PREFIX} --modules-folder ${TERMUX_PREFIX}/share/code-server/node_modules .
 }
