9427adce2ab74fee44d473305f383de7b97447a6
kawanakaiku <80381779+kawanakaiku@users.noreply.github.com>
2022-09-02 16:06:24 +0900

    Create build.sh

---
+++ packages/wine/build.sh
@@ -0,0 +1,14 @@
+TERMUX_PKG_HOMEPAGE=https://www.winehq.org/
+TERMUX_PKG_DESCRIPTION="WINE Is Not An Emulator - runs MS Windows programs"
+TERMUX_PKG_LICENSE="LGPL-2.1"
+TERMUX_PKG_MAINTAINER="@kawanakaiku"
+TERMUX_PKG_VERSION=6.0.4
+TERMUX_PKG_SRCURL=https://github.com/wine-mirror/wine/archive/refs/tags/wine-.tar.gz
+TERMUX_PKG_DEPENDS="libc++, libexpat, libpng, libzopfli, zlib"
+TERMUX_PKG_BUILD_DEPENDS=""
+TERMUX_PKG_HOSTBUILD=true
+TERMUX_PKG_EXTRA_HOSTBUILD_CONFIGURE_ARGS="--enable-win64 --with-freetype --with-gettext --disable-tests --disable-win16 --without-alsa --without-capi --without-cms --without-coreaudio --without-cups --without-curses --without-dbus --without-fontconfig --without-gphoto --without-glu --without-gnutls --without-gsm --without-gstreamer --without-hal --without-jpeg --without-krb5 --without-ldap --without-mpg123 --without-netapi --without-openal --without-opencl --without-opengl --without-osmesa --without-oss --without-pcap --without-pulse --without-png --without-sane --without-tiff --without-v4l --without-x --without-xcomposite --without-xcursor --without-xinerama --without-xinput --without-xinput2 --without-xml --without-xrandr --without-xrender --without-xshape --without-xshm --without-xslt --without-xxf86vm --without-zlib"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+enable_wineandroid_drv=no
+--with-wine-tools=${TERMUX_PKG_HOSTBUILD_DIR}/wine-tools
+"
