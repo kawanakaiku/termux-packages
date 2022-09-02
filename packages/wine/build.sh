444bbda5c1d16962dfa0818b6097fadce8fdbe50
kawanakaiku <80381779+kawanakaiku@users.noreply.github.com>
2022-09-02 17:26:22 +0900

    Update build.sh

--- packages/wine/build.sh
+++ packages/wine/build.sh
@@ -5,7 +5,7 @@ TERMUX_PKG_MAINTAINER="@kawanakaiku"
 TERMUX_PKG_VERSION=6.0.4
 TERMUX_PKG_SHA256=ca50376e3f7200493214daa5f7fd1145bfc9dd085c4814e4d502d4723e7b52a6
 TERMUX_PKG_SRCURL=https://github.com/wine-mirror/wine/archive/refs/tags/wine-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_DEPENDS="freetype, libpng"
+TERMUX_PKG_DEPENDS="freetype, libpng, libx11"
 #TERMUX_PKG_BUILD_DEPENDS=""
 TERMUX_PKG_HOSTBUILD=true
 TERMUX_PKG_EXTRA_HOSTBUILD_CONFIGURE_ARGS="--enable-win64 --without-freetype --without-gettext --disable-tests --disable-win16 --without-alsa --without-capi --without-cms --without-coreaudio --without-cups --without-curses --without-dbus --without-fontconfig --without-gphoto --without-glu --without-gnutls --without-gsm --without-gstreamer --without-hal --without-jpeg --without-krb5 --without-ldap --without-mpg123 --without-netapi --without-openal --without-opencl --without-opengl --without-osmesa --without-oss --without-pcap --without-pulse --without-png --without-sane --without-tiff --without-v4l --without-x --without-xcomposite --without-xcursor --without-xinerama --without-xinput --without-xinput2 --without-xml --without-xrandr --without-xrender --without-xshape --without-xshm --without-xslt --without-xxf86vm --without-zlib"
@@ -14,6 +14,7 @@ enable_wineandroid_drv=no
 --with-wine-tools=${TERMUX_PKG_HOSTBUILD_DIR}
 --with-freetype
 --with-png
+--with-x
 --disable-tests
 "
 
