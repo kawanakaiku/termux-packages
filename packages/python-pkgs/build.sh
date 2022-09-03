e961ab8b78b9e92f827407d72b96aa7e38628c4f
kawanakaiku <80381779+kawanakaiku@users.noreply.github.com>
2022-09-03 16:31:44 +0900

    Update build.sh

--- packages/python-pkgs/build.sh
+++ packages/python-pkgs/build.sh
@@ -461,7 +461,7 @@ termux_step_pre_configure() {
 				# with patched tools/setup_helpers/cmake.py
 				export cmake_args="
 				$( cat ${TERMUX_COMMON_CACHEDIR}/tmp_cmake_args )
-				ANDROID_NDK=${NDK}
+				-DANDROID_NDK=${NDK}
 				"
 				;;
 		esac
