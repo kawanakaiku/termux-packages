5128b02a491f2763c3f598e001efeadce684d9a1
kawanakaiku <80381779+kawanakaiku@users.noreply.github.com>
2022-09-05 12:28:24 +0900

    Update build.sh

--- packages/eatmydata/build.sh
+++ packages/eatmydata/build.sh
@@ -4,3 +4,4 @@ TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@kawanakaiku"
 TERMUX_PKG_VERSION=130
 TERMUX_PKG_SRCURL=http://deb.debian.org/debian/pool/main/libe/libeatmydata/libeatmydata_${TERMUX_PKG_VERSION}.orig.tar.gz
+TERMUX_PKG_SHA256=48731cd7e612ff73fd6339378fbbff38dd3bcf6c243593b0d9773ca0051541c0
