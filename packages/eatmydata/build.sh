02fdba55352e4db0a1cefe59b6adf46fcf017f84
kawanakaiku <80381779+kawanakaiku@users.noreply.github.com>
2022-09-05 12:27:12 +0900

    Create build.sh

---
+++ packages/eatmydata/build.sh
@@ -0,0 +1,6 @@
+TERMUX_PKG_HOMEPAGE=https://pypi.org/
+TERMUX_PKG_DESCRIPTION="Library and utilities to disable fsync and friends"
+TERMUX_PKG_LICENSE="GPL-3.0"
+TERMUX_PKG_MAINTAINER="@kawanakaiku"
+TERMUX_PKG_VERSION=130
+TERMUX_PKG_SRCURL=http://deb.debian.org/debian/pool/main/libe/libeatmydata/libeatmydata_${TERMUX_PKG_VERSION}.orig.tar.gz
