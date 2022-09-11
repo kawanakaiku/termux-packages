7b0accc9a08e41ffdad18038db932de8ead2dfa8
kawanakaiku <80381779+kawanakaiku@users.noreply.github.com>
2022-09-11 21:19:09 +0900

    Update build.sh

--- packages/vscode/build.sh
+++ packages/vscode/build.sh
@@ -3,11 +3,10 @@ TERMUX_PKG_DESCRIPTION="Visual Studio Code"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="kawanakaiku"
 TERMUX_PKG_VERSION=1.71.0
-TERMUX_PKG_SRCURL=https://github.com/microsoft/vscode/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=fb106039f73ddefdf81b393d29af6f35e262628c39007307b6748dc2ee786ea6
+TERMUX_PKG_SRCURL=https://github.com/microsoft/vscode.git
 TERMUX_PKG_DEPENDS="nodejs-lts, libx11, libsecret, libxkbfile"
 TERMUX_PKG_BUILD_DEPENDS="yarn"
-TERMUX_PKG_BUILD_IN_SRC=true
+#TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_get_source() {
 	git clone --recursive --depth=1 --branch=${TERMUX_PKG_VERSION} ${TERMUX_PKG_SRCURL} ${TERMUX_PKG_SRCDIR}
