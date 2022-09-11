211687be1a8b59b8e3621daaf007e2392f915e12
kawanakaiku <80381779+kawanakaiku@users.noreply.github.com>
2022-09-11 12:25:24 +0900

    Update build.sh

--- packages/icecream/build.sh
+++ packages/icecream/build.sh
@@ -7,7 +7,3 @@ TERMUX_PKG_SRCURL=https://github.com/icecc/icecream/releases/download/${TERMUX_P
 TERMUX_PKG_SHA256=7d750e8b866215f8b8e93404a75a8d5a9b1c1a675565af847e4d24964ec7d66b
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_DEPENDS="libcap-ng, zstd, libarchive, liblzo"
-
-termux_step_pre_configure() {
-	./autogen.sh
-}
