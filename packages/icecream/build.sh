d209f1e6212a362af88a40d7155caba12661c152
kawanakaiku <80381779+kawanakaiku@users.noreply.github.com>
2022-09-11 12:08:36 +0900

    Update build.sh

--- packages/icecream/build.sh
+++ packages/icecream/build.sh
@@ -4,6 +4,7 @@ TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@kawanakaiku"
 TERMUX_PKG_VERSION=1.4
 TERMUX_PKG_SRCURL=https://github.com/icecc/icecream/releases/download/${TERMUX_PKG_VERSION}/icecc-${TERMUX_PKG_VERSION}.0.tar.xz
+TERMUX_PKG_SHA256=7d750e8b866215f8b8e93404a75a8d5a9b1c1a675565af847e4d24964ec7d66b
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_DEPENDS="libcap-ng, zstd, libarchive, liblzo"
 
