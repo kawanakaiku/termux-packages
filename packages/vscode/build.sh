d78dbe2b97cc682b9fde4393ab427ea34703df65
kawanakaiku <80381779+kawanakaiku@users.noreply.github.com>
2022-09-11 21:28:44 +0900

    Update build.sh

--- packages/vscode/build.sh
+++ packages/vscode/build.sh
@@ -5,14 +5,16 @@ TERMUX_PKG_MAINTAINER="kawanakaiku"
 TERMUX_PKG_VERSION=1.71.0
 TERMUX_PKG_SRCURL=https://github.com/microsoft/vscode.git
 TERMUX_PKG_DEPENDS="nodejs-lts, libx11, libsecret, libxkbfile"
-TERMUX_PKG_BUILD_DEPENDS="yarn"
-#TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_get_source() {
 	git clone --recursive --depth=1 --branch=${TERMUX_PKG_VERSION} ${TERMUX_PKG_SRCURL} ${TERMUX_PKG_SRCDIR}
 }
 
 termux_step_make_install() {
+	npm install -g yarn
+	
+	touch ${TERMUX_BUILD_TS_FILE}
+	
 	yarn add --prefix ${TERMUX_PREFIX} ${TERMUX_PKG_SRCDIR}
 	yarn cache clean
 }
