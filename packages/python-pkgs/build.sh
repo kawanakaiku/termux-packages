885a5f68c9d08eb9cce8b0874af8e0e0f0766cd8
kawanakaiku <80381779+kawanakaiku@users.noreply.github.com>
2022-09-13 12:51:18 +0900

    Update build.sh

--- packages/python-pkgs/build.sh
+++ packages/python-pkgs/build.sh
@@ -847,7 +847,7 @@ termux_step_pre_configure() {
 		local REQUIRES=$(
 		case $PYTHON_PKG in
 			matplotlib ) printf 'cycler kiwisolver' ;;
-			japanize_matplotlib ) printf 'matplotlib' ;;
+			japanize-matplotlib ) printf 'matplotlib' ;;
 			homeassistant ) printf 'sqlalchemy' ;;
 			pyopenjtalk )
 				# from setup.py
