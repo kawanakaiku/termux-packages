8b2471d602c6bc9eab3e3500799a39dec0cfbf51
kawanakaiku <80381779+kawanakaiku@users.noreply.github.com>
2022-09-16 02:01:07 +0900

    Update build.sh

--- x11-packages/firefox/build.sh
+++ x11-packages/firefox/build.sh
@@ -7,6 +7,11 @@ TERMUX_PKG_SRCURL=http://archive.ubuntu.com/ubuntu/pool/main/f/firefox/firefox_$
 TERMUX_PKG_SHA256=2fca320b537741ee0307c3a4e8535e8782a299049691aa3571e15707d3582f21
 TERMUX_PKG_DEPENDS="at-spi2-atk, libcairo, dbus, dbus-glib, libffi, fontconfig, freetype, gdk-pixbuf, glib, gtk3, harfbuzz, pango, libx11, libxcb, libxcomposite, libxcursor, libxdamage, libxext, libxfixes, libxi, libxrandr, libxrender, libxtst"
 #TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--host=x86_64-pc-linux-gnu
+--target=$TERMUX_HOST_PLATFORM
+--with-android-ndk=$NDK
+"
 
 termux_step_post_get_source() {
 	cat <<-'SH' >configure
