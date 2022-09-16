a9383b457cc84af943f2c7c2e24c07af5a356d00
kawanakaiku <80381779+kawanakaiku@users.noreply.github.com>
2022-09-16 12:40:55 +0900

    Update build.sh

--- x11-packages/firefox/build.sh
+++ x11-packages/firefox/build.sh
@@ -16,10 +16,9 @@ _termux_step_post_get_source() {
 termux_step_pre_configure() {
 	unset RUSTFLAGS
 	
-	sed -i '1s/^/target.termux_os="Linux"\n/' build/moz.configure/init.configure
 	find "$TERMUX_PKG_SRCDIR" -type f -name '*.configure' | \
 		xargs -n 1 sed -i \
-		-e 's|\([^_]\)target\.os|\target.termux_os|g'
+		-e 's|"Android"|"_NO_TERMUX_Android"|g'
 	#sed -i -e '/=arm_option_defaults\./d' build/moz.configure/arm.configure
 }
 
