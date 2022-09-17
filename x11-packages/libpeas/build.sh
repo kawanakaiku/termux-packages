7a8e3001368785987310eefc172771d4c02e865e
kawanakaiku <80381779+kawanakaiku@users.noreply.github.com>
2022-09-18 03:43:14 +0900

    Update build.sh

--- x11-packages/libpeas/build.sh
+++ x11-packages/libpeas/build.sh
@@ -20,7 +20,7 @@ termux_step_pre_configure() {
 	# Couldn't find include 'GObject-2.0.gir'
 	wget -nv https://github.com/kawanakaiku/test-ci/releases/download/src/gir-1.0.zip
 	mkdir -p /usr/share/gir-1.0
-	unzip -q gir-1.0.zip -p /usr/share/gir-1.0
+	unzip gir-1.0.zip -q -d /usr/share/gir-1.0
 }
 
 _termux_step_post_make_install() {
