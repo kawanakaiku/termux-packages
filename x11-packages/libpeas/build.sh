7a38b9de4e36eaacdef3490ad94f0542ee4c9a9a
kawanakaiku <80381779+kawanakaiku@users.noreply.github.com>
2022-09-18 02:50:05 +0900

    Update build.sh

--- x11-packages/libpeas/build.sh
+++ x11-packages/libpeas/build.sh
@@ -7,10 +7,14 @@ TERMUX_PKG_SRCURL=https://download.gnome.org/sources/libpeas/${TERMUX_PKG_VERSIO
 TERMUX_PKG_SHA256=d625520fa02e8977029b246ae439bc218968965f1e82d612208b713f1dcc3d0e
 TERMUX_PKG_DEPENDS="gobject-introspection, glib, gtk3"
 
-termux_step_pre_configure() {
+_termux_step_pre_configure() {
 	# OSError: [Errno 8] Exec format error: '/data/data/com.termux/files/usr/bin/g-ir-scanner'
 	cat <<-SH >$TERMUX_PREFIX/bin/g-ir-scanner
 	#!$TERMUX_PREFIX/bin/sh
 	exit 0
 	SH
+}
+
+_termux_step_post_make_install() {
+    rm $TERMUX_PREFIX/bin/g-ir-scanner
 }
