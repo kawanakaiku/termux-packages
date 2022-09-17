f53399ae8071cb7aa95f264f14100ee3cd923625
kawanakaiku <80381779+kawanakaiku@users.noreply.github.com>
2022-09-18 02:26:50 +0900

    Update build.sh

--- x11-packages/libpeas/build.sh
+++ x11-packages/libpeas/build.sh
@@ -5,4 +5,5 @@ TERMUX_PKG_MAINTAINER="kawanakaiku"
 TERMUX_PKG_VERSION=1.32.0
 TERMUX_PKG_SRCURL=https://download.gnome.org/sources/libpeas/${TERMUX_PKG_VERSION%.*}/libpeas-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=d625520fa02e8977029b246ae439bc218968965f1e82d612208b713f1dcc3d0e
-TERMUX_PKG_DEPENDS="gobject-introspection, glib, gtk3"
+TERMUX_PKG_DEPENDS="glib, gtk3"
+#gobject-introspection
