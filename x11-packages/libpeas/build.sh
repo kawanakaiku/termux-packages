9f8d24f301b1d9b942b67f46d6fa94b352266e3c
kawanakaiku <80381779+kawanakaiku@users.noreply.github.com>
2022-09-18 02:14:55 +0900

    Create build.sh

---
+++ x11-packages/libpeas/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=https://wiki.gnome.org/Projects/Libpeas
+TERMUX_PKG_DESCRIPTION="Application plugin library"
+TERMUX_PKG_LICENSE="GPL-3.0"
+TERMUX_PKG_MAINTAINER="kawanakaiku"
+TERMUX_PKG_VERSION=1.32.0
+TERMUX_PKG_SRCURL=https://download.gnome.org/sources/libpeas/${TERMUX_PKG_VERSION%.*}/libpeas-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=d625520fa02e8977029b246ae439bc218968965f1e82d612208b713f1dcc3d0e
+TERMUX_PKG_DEPENDS="gobject-introspection, glib, gtk3"
