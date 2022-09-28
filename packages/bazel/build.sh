3c9342e759d8efeed98b3e235f01691fe9d2a364
kawanakaiku <80381779+kawanakaiku@users.noreply.github.com>
2022-09-28 09:28:30 +0900

    Update build.sh

--- packages/bazel/build.sh
+++ packages/bazel/build.sh
@@ -7,6 +7,13 @@ TERMUX_PKG_SRCURL=https://github.com/bazelbuild/bazel/releases/download/${TERMUX
 TERMUX_PKG_SHA256=18486e7152ca26b26585e9b2a6f49f332b116310d3b7e5b70583f1f1f24bb8ae
 TERMUX_PKG_BUILD_IN_SRC=true
 
+termux_step_get_source() {
+  local file="$TERMUX_PKG_CACHEDIR/$(basename "${PKG_SRCURL[$i]}")"
+  termux_download "$TERMUX_PKG_SRCURL" "$file" "$TERMUX_PKG_SHA256"
+  mkdir "$TERMUX_PKG_SRCDIR"
+  unzip "$file" -d "$TERMUX_PKG_SRCDIR"
+}
+
 termux_step_make() {
   env EXTRA_BAZEL_ARGS="--tool_java_runtime_version=local_jdk" bash ./compile.sh
 }
