089616e2a45584e83355d3e0182db0721295d984
kawanakaiku <80381779+kawanakaiku@users.noreply.github.com>
2022-10-01 17:09:13 +0900

    Update build.sh

--- packages/python-torch/build.sh
+++ packages/python-torch/build.sh
@@ -5,6 +5,13 @@ TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.12.1
 TERMUX_PKG_SRCURL=https://github.com/pytorch/pytorch.git
 TERMUX_PKG_DEPENDS="python, python-numpy, libprotobuf, libopenblas"
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_HOSTBUILD=true
+
+termux_step_host_build() {
+	cmake "$TERMUX_PKG_SRCDIR/third_party/sleef"
+	make -j "$TERMUX_MAKE_PROCESSES"
+}
 
 termux_step_pre_configure() {
 	_PYTHON_VERSION=$(. $TERMUX_SCRIPTDIR/packages/python/build.sh; echo $_MAJOR_VERSION)
@@ -22,6 +29,14 @@ termux_step_pre_configure() {
 	termux_setup_protobuf
 	
 	build-pip install pyyaml
+
+	find . -name CMakeLists.txt -o -name '*.cmake' | \
+		xargs -n 1 sed -i \
+		-e 's/\([^A-Za-z0-9_]ANDROID\)\([^A-Za-z0-9_]\)/\1_NO_TERMUX\2/g' \
+		-e 's/\([^A-Za-z0-9_]ANDROID\)$/\1_NO_TERMUX/g'
+	
+	sed -i "s|np.get_include()|'${TERMUX_PREFIX}/lib/python${_PYTHON_VERSION}/site-packages/numpy/core/include'|" tools/setup_helpers/numpy_.py	
+	sed -i 's/**build_options,/**build_options | {i: j for i, j in [i.strip().split("=", 1) for i in os.getenv("termux_cmake_args").split(os.linesep) if "=" in i]},/' tools/setup_helpers/cmake.py
 }
 
 termux_step_configure() {
@@ -33,6 +48,47 @@ termux_step_make() {
 }
 
 termux_step_make_install() {
+	export OpenBLAS_HOME=${TERMUX_PREFIX}
+	export MAX_JOBS=${TERMUX_MAKE_PROCESSES}
+        export termux_cmake_args="
+        ANDROID=0
+	USE_VULKAN=0
+	USE_CUDA=0
+	USE_CUDNN=0
+	USE_MKLDNN=0
+	USE_DISTRIBUTED=0
+	USE_NINJA=0
+	USE_NUMPY=1
+	USE_BLAS=1
+	BLAS=OpenBLAS
+	USE_ZMQ=0
+	USE_FFMPEG=0
+	USE_LMDB=0
+	USE_LEVELDB=0
+	USE_GFLAGS=0
+	USE_FFTW=0
+	USE_OPENMP=0
+	USE_TBB=0
+	USE_SYSTEM_TBB=0
+	USE_ROCKSDB=0
+	USE_SYSTEM_SLEEF=0
+	BUILD_TEST=0
+	BUILD_PYTHON=1
+	BUILD_CUSTOM_PROTOBUF=0
+	CAFFE2_LINK_LOCAL_PROTOBUF=1
+	ANDROID_NO_TERMUX=OFF
+	NATIVE_BUILD_DIR=${TERMUX_PKG_HOSTBUILD_DIR}
+	PROTOBUF_PROTOC_EXECUTABLE=$(command -v protoc)
+	CAFFE2_CUSTOM_PROTOC_EXECUTABLE=$(command -v protoc)
+	OPENMP_FLAG=-fopenmp -static-openmp
+	INTERN_BUILD_MOBILE=1
+	BUILD_CAFFE2=1
+	BUILD_CAFFE2_MOBILE=1
+	UNIX=1
+	USE_FBGEMM=0
+        "
+	LDFLAGS+=" -llog"
+	
 	cross-pip -v install $TERMUX_PKG_SRCDIR
 }
 
