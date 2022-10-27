f3e4b391536096d04fa5f2cf9ed052cbabd54799
kawanakaiku <80381779+kawanakaiku@users.noreply.github.com>
2022-10-27 09:55:12 +0900

    Update build.sh

--- x11-packages/freebasic/build.sh
+++ x11-packages/freebasic/build.sh
@@ -10,3 +10,14 @@ TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_MAKE_ARGS="
 TARGET=$TERMUX_HOST_PLATFORM
 "
+
+termux_step_pre_configure() {
+termux_download \
+https://sourceforge.net/projects/fbc/files/FreeBASIC-1.09.0/Binaries-Linux/FreeBASIC-1.09.0-linux-x86_64.tar.xz \
+${TERMUX_PKG_TMPDIR}/FreeBASIC-linux-x86_64.tar.xz \
+c8143b1207541ddcdf8d5d87f36a85a8bf6fdf85b3e6aee56323a0ecdacbc1e5
+_HOST_FBC=${TERMUX_PKG_TMPDIR}/host-fbc
+mkdir $_HOST_FBC
+tar xf FreeBASIC-linux-x86_64.tar.xz -C $_HOST_FBC --strip-components 1
+PATH="$_HOST_FBC/bin:$PATH"
+}
