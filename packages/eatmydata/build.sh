bbf043acc9cf91908658cc7e5aacfc9ec5ccf0de
kawanakaiku <80381779+kawanakaiku@users.noreply.github.com>
2022-11-13 02:14:07 +0900

    Update build.sh

--- packages/eatmydata/build.sh
+++ packages/eatmydata/build.sh
@@ -7,10 +7,14 @@ TERMUX_PKG_SHA256=48731cd7e612ff73fd6339378fbbff38dd3bcf6c243593b0d9773ca0051541
 TERMUX_PKG_SRCURL=https://github.com/stewartsmith/libeatmydata/releases/download/v${TERMUX_PKG_VERSION}/libeatmydata-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_BUILD_IN_SRC=true
 
-termux_step_pre_configure() {
+_termux_step_pre_configure() {
 	sed -i -e '1i #include <stdatomic.h>\nstatic atomic_flag event_thread_cancel;' libeatmydata/libeatmydata.c
 	sed -i -e 's|if (pthread_testcancel)|if (!atomic_flag_test_and_set(\&event_thread_cancel))|' libeatmydata/libeatmydata.c
 	sed -i -e 's|pthread_testcancel();|pthread_exit(NULL);|' libeatmydata/libeatmydata.c
+}
+
+termux_step_pre_configure() {
+	sed -i -e '/pthread_testcancel/d' libeatmydata/libeatmydata.c
 }
 
 termux_step_post_make_install() {
